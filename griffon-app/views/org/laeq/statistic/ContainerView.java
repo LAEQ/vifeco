c7829b4af6b4684ca424bdb319e56fd3c1c324f4
David Maignan <david@localhost.localdomain>
2019-03-23 17:44:20 -0400

    Display statistic result

--- griffon-app/views/org/laeq/statistic/ContainerView.java
+++ griffon-app/views/org/laeq/statistic/ContainerView.java
@@ -5,11 +5,10 @@ import griffon.inject.MVCMember;
 import griffon.metadata.ArtifactProviderFor;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.util.Callback;
 import javafx.util.Duration;
@@ -18,14 +17,15 @@ import org.apache.commons.collections4.bidimap.DualHashBidiMap;
 import org.codehaus.griffon.runtime.javafx.artifact.AbstractJavaFXGriffonView;
 import org.laeq.model.Category;
 import org.laeq.model.Video;
-import org.laeq.model.statistic.Vertex;
 import org.laeq.service.statistic.StatisticException;
 import org.laeq.service.statistic.StatisticService;
 import org.laeq.template.MiddlePaneView;
 import org.laeq.ui.DialogService;
-
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -55,6 +55,26 @@ public class ContainerView extends AbstractJavaFXGriffonView {
     }
 
     public void init(){
+        durationSlider.setMin(0);
+        durationSlider.setMax(60);
+        durationSlider.setMajorTickUnit(10);
+        durationSlider.setShowTickMarks(true);
+        durationSlider.setShowTickLabels(true);
+
+        durationSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            try{
+                BigDecimal bd = new BigDecimal(newValue.toString());
+                bd = bd.setScale(0, RoundingMode.HALF_EVEN);
+                durationSlider.setValue(bd.intValueExact());
+
+                compare(bd.intValueExact());
+
+            } catch (Exception e){
+                getLog().error(e.getMessage());
+            }
+
+        });
+
         selectBoxes = new DualHashBidiMap<>();
 
         TableColumn<Video, Void> selectBox = new TableColumn<>("#");
@@ -78,7 +98,7 @@ public class ContainerView extends AbstractJavaFXGriffonView {
 
         compareBtn.setOnMouseClicked(event -> {
             //Validate video selection
-            compare();
+            compare((int) durationSlider.getValue());
         });
 
         gridResult.setPadding(new Insets(10,10,10,10));
@@ -87,42 +107,87 @@ public class ContainerView extends AbstractJavaFXGriffonView {
 
     }
 
-    private void compare(){
+    private void compare(int step){
+        gridResult.getChildren().clear();
+        gridResult.getColumnConstraints().clear();
+
+        gridResult.add(new Label("Category"), 0,0);
+        gridResult.add(new Label("Video A - Total"), 1,0);
+        gridResult.add(new Label("Delta A"), 2,0);
+        gridResult.add(new Label("% "), 3,0);
+        gridResult.add(new Label("Video B - Total"), 4,0);
+        gridResult.add(new Label("Delta B"), 5,0);
+        gridResult.add(new Label("%"), 6,0);
+
+        gridResult.getColumnConstraints().addAll(getColumnConstraints());
+
+
+
         Video video1 = this.model.getVideos().get(0);
         Video video2 = this.model.getVideos().get(1);
 
         try {
             statService.setVideos(video1, video2);
-            statService.setDurationStep(Duration.seconds(10));
-            Map<Category, List<List<Vertex>>> result = statService.execute();
-
-            System.out.println(result);
-
-            int rowIndex = 1;
-            int columnIndex = 0;
-
-            for(Map.Entry<Category, List<List<Vertex>>> entry: result.entrySet()){
-                gridResult.add(new Label(entry.getKey().getName()), columnIndex, rowIndex);
-
-                List<List<Vertex>> vertices = entry.getValue();
-
-                int[] totalA = new int[]{0};
-                int[] totalB = new int[]{0};
+            statService.setDurationStep(Duration.seconds(step));
+            Map<Category, Map<Video, Long>> result = statService.analyse();
 
 
+            final int[] rowIndex = new int[]{1};
+
+            result.entrySet().forEach(e -> {
+                gridResult.add(new Label(e.getKey().getName()), 0, rowIndex[0]);
+
+                long totalA = video1.getPointSet().stream().filter(point -> point.getCategory().equals(e.getKey())).count();
+
+                gridResult.add(new Label(String.valueOf(totalA)), 1, rowIndex[0]);
+                gridResult.add(new Label(e.getValue().get(video1).toString()), 2, rowIndex[0]);
+                gridResult.add(new Label(rounder((e.getValue().get(video1) / ((double)totalA) * 100)) + "%"), 3, rowIndex[0]);
+
+                long totalB = video2.getPointSet().stream().filter(point -> point.getCategory().equals(e.getKey())).count();
+
+                gridResult.add(new Label(String.valueOf(totalB)), 4, rowIndex[0]);
+                gridResult.add(new Label(e.getValue().get(video2).toString()), 5, rowIndex[0]);
+                gridResult.add(new Label(rounder(e.getValue().get(video2) / ((double)totalB) * 100) + "%"), 6, rowIndex[0]);
+                rowIndex[0]++;
+            });
 
 
-
-
-
-
-                columnIndex = 0;
-                rowIndex++;
-            }
 
         }catch (StatisticException e){
             dialogService.simpleAlert("key.title.error", e.getMessage());
         }
+    }
+
+    private String rounder(double value){
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(0, RoundingMode.HALF_EVEN);
+
+        return bd.toString();
+    }
+
+    private List<ColumnConstraints> getColumnConstraints(){
+
+        List<ColumnConstraints> colConstList = new ArrayList<>();
+
+        ColumnConstraints colConst1 = new ColumnConstraints();
+        colConst1.setMinWidth(200);
+        colConst1.setPrefWidth(250);
+        colConst1.setMaxWidth(300);
+        colConstList.add(colConst1);
+
+        ColumnConstraints colConst2 = new ColumnConstraints();
+        colConst2.setMinWidth(50);
+        colConst2.setPrefWidth(100);
+        colConst2.setMaxWidth(200);
+
+        colConstList.add(colConst2);
+        colConstList.add(colConst2);
+        colConstList.add(colConst2);
+        colConstList.add(colConst2);
+        colConstList.add(colConst2);
+        colConstList.add(colConst2);
+
+        return colConstList;
     }
 
     private Callback<TableColumn<Video, Void>, TableCell<Video, Void>> addSelectBox() {
