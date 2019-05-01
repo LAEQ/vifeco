cb0abe98d03367bd0f2ae97363dfcbf1db3c6cc9
David Maignan <davidmaignan@gmail.com>
2019-05-01 15:39:04 -0400

    Update readme

--- src/main/java/org/laeq/model/serializer/VideoSerializer.java
+++ src/main/java/org/laeq/model/serializer/VideoSerializer.java
@@ -1,7 +1,6 @@
 package org.laeq.model.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import org.laeq.model.Video;
