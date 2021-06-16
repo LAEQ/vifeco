21b3b5b16a31d9c066560575dfccbba8b4fd6a0f
David Maignan <davidmaignan@gmail.com>
2021-06-16 10:03:13 -0400

    Actions configuration

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -4,6 +4,9 @@ echo "HOME: $HOME"
 echo "JAVA_HOME: $JAVA_HOME"
 echo "GRADLE_HOME: $GRADLE_HOME"
 
+java -version
+gradle -version
+
 ls -la /root/
 
 bash -c "echo Hello world my name is $INPUT_MY_NAME"
